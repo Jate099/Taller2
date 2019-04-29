bb8eae8cd6be67c2a19eaf1e9a19a7a7488738b9
Javier Trochez <jtrochez.jate@gmail.com>
2019-04-29 08:31:14 -0500

    se inicializa la base de datos de mongo y se instala en el proyecto

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ var express = require('express');
 var renderEngine = require('express-handlebars');
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'tienda';
+const client = new MongoClient(url);
+
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  const productos = db.collection('productos');
+  productos.find({}, {sort: ['precio']}).toArray(function(err, docs){
+
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs.length);
+    
+    docs.forEach(function(prod){
+      console.log(prod.precio);
+    });
+  });
+
+  client.close();
+});
+
 app.use(express.static('public'));
 app.set('view engine', 'handlebars');
 app.engine('handlebars', renderEngine());
@@ -20,7 +47,6 @@ productos.push({
   imagen: 'https://www.infobae.com/new-resizer/kAjCyEfwdw0H57sLGDM5OOrTFUI=/750x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/04/06155038/perro-beso-1024x576.jpg',
   descripcion: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia sapiente iste, atque, cumque vero expedita consectetur, voluptatum soluta molestias ipsam natus veritatis est blanditiis corporis eveniet vel perspiciatis aspernatur quas?',
 });
-
 
 app.get('/', function (req, response) {
   response.sendFile(__dirname + '/public/landing.html');
