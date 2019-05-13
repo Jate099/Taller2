9b86158e7451b206baa4b2308ad331ed2b02ed9a
Jate099 <42478567+Jate099@users.noreply.github.com>
2019-05-13 03:30:42 -0500

    se hace la lista del carrito completa, ahora se eliminan los productos que uno seleccione

--- public/script/categoria.js
+++ public/script/categoria.js
@@ -1,68 +1,121 @@
-function cargarPag(){
+function cargarPag() {
 
+    //-----------------menu hamburguesa----------------------------------------
     btnHamburguesa = document.querySelector('.sideMenu__menu-btn');
     cerrarMenu = document.querySelector('.sideMenu__cerrar');
     menu = document.querySelector('.sideMenu');
     main = document.querySelector('.categoria');
 
-    function openSlidebar(){
+    function openSlidebar() {
         menu.style.left = '0';
         main.style.marginLeft = '200px';
         btnHamburguesa.style.display = 'none';
     }
     btnHamburguesa.addEventListener('click', openSlidebar);
 
-    function closeSlidebar(){
-            menu.style.left = '-200px';
-            main.style.marginLeft = '0';
-            btnHamburguesa.style.display = 'block';
+    function closeSlidebar() {
+        menu.style.left = '-200px';
+        main.style.marginLeft = '0';
+        btnHamburguesa.style.display = 'block';
     }
     cerrarMenu.addEventListener('click', closeSlidebar);
 
+    //-----------------menu carrito----------------------------------------
     btnCart = document.querySelector('.cart__carrito-btn');
     cartCerrar = document.querySelector('.cart__cerrar');
     menuCart = document.querySelector('.cart');
 
-    function openCart(){
+    function openCart() {
         menuCart.style.right = '0';
         //main.style.marginRight = '300px';
         //btnHamburguesa.style.display = 'none';
     }
     btnCart.addEventListener('click', openCart);
-    
-    function closeCart(){
+
+    function closeCart() {
         menuCart.style.right = '-300px';
         //main.style.marginRight = '0';
         //btnHamburguesa.style.display = 'block';
     }
     cartCerrar.addEventListener('click', closeCart);
 
+    //-----------------filtro precio----------------------------------------
     var rango = document.querySelector('.input-rango');
 
-    function buscarPorPrecio(){
+    function buscarPorPrecio() {
         console.log(rango.value);
         location.href = '/tienda/headwear?precio=' + rango.value;
     }
-    if(rango != null){
+    if (rango != null) {
         rango.addEventListener('change', buscarPorPrecio);
     }
 
+    //-----------------carrito de compras----------------------------------------
     //botones del carrito en la pagina de categoria
-    var listaProducto = [];
-    if(localStorage.getItem('listaProducto') != null){
-        listaProducto = JSON.parse(localStorage.getItem('listaProducto'));
-    }
-
     var carrito = document.querySelector('.cart__cant');
     //lista de productos en el carrito
     var listaCarrito = document.querySelector('.cart__cartMenu-lista');
+    var listaProducto = [];
 
-    function actualizarCarrito(){
+    if (localStorage.getItem('listaProducto') != null) {
+        listaProducto = JSON.parse(localStorage.getItem('listaProducto'));
+    }
+
+    function actualizarCarrito() {
+        var suma = 0;
+
+        //numero de elementos en el carrito
         carrito.innerHTML = listaProducto.length;
 
-        listaCarrito.innerHTML = '';
-        listaProducto.forEach(function(producto){
-            listaCarrito.innerHTML += '<div class="cart__item"><img class="cart__img" src="' + producto.imagenes + '" width="50"><p class="cart__nombre">'+ producto.nombre +'</p><p class="cart__precio">'+ producto.precio +'</p></div>';
+        if (listaCarrito != null) {
+            listaCarrito.innerHTML = '';
+        }
+
+        listaProducto.forEach(function (producto, index) {
+            //listaCarrito.innerHTML += '<div class="cart__item"><img class="cart__img" src="' + producto.imagenes + '" width="50"><p class="cart__nombre">'+ producto.nombre +'</p><p class="cart__precio">'+ producto.precio +'</p></div>';
+
+            var contItem = document.createElement('div');
+            var imgNuevo = document.createElement('div');
+            var nombreNuevo = document.createElement('p');
+            var precioNuevo = document.createElement('p');
+            var eliminarProd = document.createElement('button');
+
+            if (listaCarrito != null) {
+                listaCarrito.appendChild(contItem);
+                contItem.appendChild(imgNuevo);
+                contItem.appendChild(nombreNuevo);
+                contItem.appendChild(precioNuevo);
+                contItem.appendChild(eliminarProd);
+            }
+
+            contItem.className = 'cart__item';
+            imgNuevo.className = 'cart__img';
+            nombreNuevo.className = 'cart__nombre';
+            precioNuevo.className = 'cart__precio';
+            eliminarProd.className = 'cart__btn-eliminar'
+
+            imgNuevo.style.backgroundImage = 'url(' + producto.imagenes + ')';
+            nombreNuevo.innerHTML = producto.nombre;
+            precioNuevo.innerHTML = producto.precio;
+            eliminarProd.innerHTML = 'eliminar';
+            /*cantNuevo.innerHTML = ""+cantidad;
+            btnNuevo.innerHTML = '-';
+            btnNuevo2.innerHTML = '+';*/
+
+            //eliminar producto del carrito
+            function eliminarProdCart() {
+                
+                listaProducto.splice(index, 1);
+                contItem.remove();
+                localStorage.setItem('listaProducto', JSON.stringify(listaProducto));
+                //carrito.innerHTML = listaProducto.length;
+                actualizarCarrito();
+
+            }
+            if (eliminarProd != null) {
+                eliminarProd.addEventListener('click', eliminarProdCart);
+            }
+
         });
     }
     actualizarCarrito();
@@ -71,8 +124,8 @@ function cargarPag(){
 
     var carritoBtn = document.querySelectorAll('.item__carritoBtn');
 
-    function recorreBotones(boton){
-        function agergarCarrito(){
+    function recorreBotones(boton) {
+        function agergarCarrito() {
             console.log('funcionando')
             var padre = boton.parentNode;
             var nombre = padre.querySelector('.item__nombre').innerText;
@@ -101,31 +154,32 @@ function cargarPag(){
     var btnMenos = document.querySelector('.cantidad__menos');
 
     //funcion para los botones de ageragr al carrito en la pagina producto
-    function agrgarCartProduct(){
+    function agrgarCartProduct() {
         console.log('funcionando')
-            var nombre = document.querySelector('.encabezado__titulo').innerText;
-            var precio = document.querySelector('.encabezado__precio').innerText;
-            //var imagen = padre.querySelector('.preview__principal').src;
+        var nombre = document.querySelector('.encabezado__titulo').innerText;
+        var precio = document.querySelector('.encabezado__precio').innerText;
+        //var imagen = padre.querySelector('.preview__principal').src;
 
-            var producto = {
-                nombre: nombre,
-                precio: precio,
-                //imagen: imagen,
-            }
+        var producto = {
+            nombre: nombre,
+            precio: precio,
+            //imagen: imagen,
+        }
 
-            listaProducto.push(producto);
+        listaProducto.push(producto);
 
-            //carrito.innerHTML = listaProducto.length;
-            actualizarCarrito();
+        //carrito.innerHTML = listaProducto.length;
+        actualizarCarrito();
 
-            localStorage.setItem('listaProducto', JSON.stringify(listaProducto));
+        localStorage.setItem('listaProducto', JSON.stringify(listaProducto));
     }
-    if(botonPorduct != null && btnSumar != null){
+    if (botonPorduct != null && btnSumar != null) {
         botonPorduct.addEventListener('click', agrgarCartProduct);
         btnSumar.addEventListener('click', agrgarCartProduct);
     }
 
-    function eliminarProdCart(){
+    //eliminar producto del carrito
+    /*function eliminarProdCart(){
         var nombre = document.querySelector('.encabezado__titulo').innerText;
             var precio = document.querySelector('.encabezado__precio').innerText;
             //var imagen = padre.querySelector('.preview__principal').src;
@@ -145,12 +199,12 @@ function cargarPag(){
     }
     if(btnMenos != null){
         btnMenos.addEventListener('click', eliminarProdCart);
-    }
+    }*/
 
     var cantidad = document.querySelector('.cantidad__contador');
     var contador = 0;
 
-    function sumarProd(){
+    function sumarProd() {
         contador++;
 
         cantidad.innerHTML = contador;
@@ -159,13 +213,13 @@ function cargarPag(){
     }
     btnSumar.addEventListener('click', sumarProd);
 
-    function restarProd(){
+    function restarProd() {
         contador--;
 
         cantidad.innerHTML = contador;
         console.log(contador);
     }
     btnMenos.addEventListener('click', restarProd);
-    
+
 }
 window.addEventListener('load', cargarPag);
