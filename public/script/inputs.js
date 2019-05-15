859ba65aa950ae646972e312e1ee9eb6a97ed792
Jate099 <42478567+Jate099@users.noreply.github.com>
2019-05-14 19:22:10 -0500

    se hace el estilo del formulario del checkout

---
+++ public/script/inputs.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function(){
+
+    $(function () {
+ 
+        $(".field-wrapper .field-placeholder").on("click", function () {
+            $(this).closest(".field-wrapper").find("input").focus();
+        });
+        $(".field-wrapper input").on("keyup", function () {
+            var value = $.trim($(this).val());
+            if (value) {
+                $(this).closest(".field-wrapper").addClass("hasValue");
+            } else {
+                $(this).closest(".field-wrapper").removeClass("hasValue");
+            }
+        });
+
+    });
+});
